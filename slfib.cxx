41ab4a7b43db55a19be42ddc70cb22ac769d71ba
DvlshSftwr <71295247+DvlshSftwr@users.noreply.github.com>
2020-10-07 04:50:32 -0400

    Update slfib.cxx

--- slfib.cxx
+++ slfib.cxx
@@ -13,13 +13,13 @@ const short       MAX { 100 },
 
 
 void fizzBuzzer(short x, short y, short z);
+void makeInteractive(void);
+void laterHater(void);
+void clearScreen(short OS);
 
 
 int main (int argCnt, char** argVec) {
-    short num1 { MIN },
-          num2 { MIN },
-          num3 { MIN },
-          OS   { MIN };
+    short OS   { MIN };
     
     std::cout << "DOS or NIX?(1/2) : ";
     std::cin >> OS;
@@ -28,27 +28,14 @@ int main (int argCnt, char** argVec) {
     
         if (OS < 1 || OS > 2)
             return EXIT_FAILURE;
-    
-        std::cout << "Enter 3 numbers to fizz, buzz, or fizzbuzz by (ex: 3 5 15) : "; 
-    
-        std::cin >> num1;
-        std::cin >> num2;
-        std::cin >> num3;
-    
-        fizzBuzzer(num1, num2, num3);
+        
+        makeInteractive();
     }
     else if (strcmp(argVec[1], "-s") == 0 || strcmp(argVec[1], "--static") == 0) 
         fizzBuzzer(3, 5, 15);
     
-    std::cout << "[{( Later Hater )}]\nPress Enter To Exit\n";
-    std::cin.get();
-    std::cin.get();
-    
-    if (OS == 1)
-        system(DOS.c_str());
-
-    else if (OS = 2)
-        system(NIX.c_str());
+    laterHater();
+    clearScreen();
     
     return EXIT_SUCCESS;
 }
@@ -69,4 +56,34 @@ void fizzBuzzer(short x, short y, short z) {
         
         else std::cout << cnt << std::endl;
     }
+}
+
+
+void makeInteractive(void) {
+    short num1 { MIN },
+          num2 { MIN },
+          num3 { MIN };
+    
+    std::cout << "Enter 3 numbers to fizz, buzz, or fizzbuzz by (ex: 3 5 15) : "; 
+    
+    std::cin >> num1;
+    std::cin >> num2;
+    std::cin >> num3;
+    
+    fizzBuzzer(num1, num2, num3);
+}
+
+
+void laterHater(void) {
+    std::cout << "[{( Later Hater )}]\nPress Enter To Exit\n";
+    std::cin.get();
+    std::cin.get();
+}
+
+void clearScreen(short OS) {
+    if (OS == 1)
+        system(DOS.c_str());
+
+    else if (OS = 2)
+        system(NIX.c_str());
 }
